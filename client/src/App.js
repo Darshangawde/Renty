18aaa04ad6529f404013a4d83e4ca0f391a72e95
Darshan Gawde <darshangawde999@gmail.com>
2022-04-19 10:45:39 +0530

    Revert "Revert "Merge pull request #9 from Darshangawde/maintenance""
    This reverts commit 279fa6be886351753c8ac60228667bcad9803f84.

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,56 @@
-import './App.css';
-import {Route , BrowserRouter , Redirect} from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import BookingCar from './pages/BookingCar'
-import 'antd/dist/antd.css';
-import UserBookings from './pages/UserBookings';
-import AddCar from './pages/AddCar';
-import AdminHome from './pages/AdminHome';
-import EditCar from './pages/EditCar';
-import AdminLogin from './pages/AdminLogin';
+import "./App.css";
+import { Route, BrowserRouter, Redirect } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import BookingCar from "./pages/BookingCar";
+import "antd/dist/antd.css";
+import UserBookings from "./pages/UserBookings";
+import AddCar from "./pages/AddCar";
+import AdminHome from "./pages/AdminHome";
+import EditCar from "./pages/EditCar";
+import AdminLogin from "./pages/AdminLogin";
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
- 
-        <BrowserRouter>
-            <Route path='/adminlogin' exact component={AdminLogin} />
-            <Route path='/login' exact component={Login} />
-            <Route path='/register' exact component={Register} />
-        </BrowserRouter>
-             
-        <BrowserRouter>
-            <AdminRoute path='/addcar' exact component={AddCar} />
-            <AdminRoute path='/editcar/:carid' exact component={EditCar} />
-            <AdminRoute path='/admin' exact component={AdminHome} />
-        </BrowserRouter>
-         
-        <BrowserRouter>
-            <ProtectedRoute path='/' exact component={Home} />
-            <ProtectedRoute path='/booking/:carid' exact component={BookingCar} />
-            <ProtectedRoute path='/userbookings' exact component={UserBookings} />
-        </BrowserRouter>
+      <BrowserRouter>
+        <Route path="/adminlogin" exact component={AdminLogin} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/register" exact component={Register} />
+        <ProtectedRoute path="/aboutus" exact component={AboutUs} />
+      </BrowserRouter>
 
+      <BrowserRouter>
+        <AdminRoute path="/addcar" exact component={AddCar} />
+        <AdminRoute path="/editcar/:carid" exact component={EditCar} />
+        <AdminRoute path="/admin" exact component={AdminHome} />
+      </BrowserRouter>
+
+      <BrowserRouter>
+        <ProtectedRoute path="/" exact component={Home} />
+        <ProtectedRoute path="/booking/:carid" exact component={BookingCar} />
+        <ProtectedRoute path="/userbookings" exact component={UserBookings} />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
 
-export function ProtectedRoute(props)
-{
-    if(localStorage.getItem('user'))
-    {
-      return <Route {...props}/>
-    }
-    else{
-      return <Redirect to='/login'/>
-    }
+export function ProtectedRoute(props) {
+  if (localStorage.getItem("user")) {
+    return <Route {...props} />;
+  } else {
+    return <Redirect to="/login" />;
+  }
 }
 
-export function AdminRoute(props)
-{
-    if(localStorage.getItem('admin'))
-    {
-      return <Route {...props}/>
-    }
-    else{
-      return <Redirect to='/adminlogin'/>
-    }
+export function AdminRoute(props) {
+  if (localStorage.getItem("admin")) {
+    return <Route {...props} />;
+  } else {
+    return <Redirect to="/adminlogin" />;
+  }
 }
---
+++ client/src/pages/AboutUs.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="abt">
+      
+      <h1>About US</h1>
+<br />
+<br />
+
+      <p>
+        Darshan - The project was built under the guidance of the team leader.
+        His rigorous support for all the members in coding their part was a
+        great effort on his part.
+      </p>
+
+      <br/>
+      <br/>
+
+      <p>
+        Kushal - The project business logic was all his ideas with the correct
+        implementation . He worked hard in making code more understandable and
+        logically accurate.
+      </p>
+
+<br/>
+<br/>
+
+      <p >
+        Pratik - The project Admin Panel was entirely coded by his ideas and
+        research work. The project got an exquisite option of authenticated
+        editing.
+      </p>
+
+<br/>
+<br/>
+
+      <p >
+        Kajal - The draft concept for the project was designed by her. She
+        implemented a car type aesthetic to the project and some other logic of
+        connecting different models.
+      </p>
+
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+
+      <h1>Contact Us</h1>
+      <p >
+        Email : darshan999@gmail.com Write to us if you want to add your cars
+        for rental
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
